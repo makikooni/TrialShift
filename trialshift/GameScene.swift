12984b651f9305132c694c16b6faabbc2e571a72
Weronika E. Falinska <w.e.falinska@gmail.com>
2024-03-29 20:54:02 +0000

    startscreen_graphics

--- trialshift/GameScene.swift
+++ trialshift/GameScene.swift
@@ -21,7 +21,7 @@ class GameScene: SKScene {
         self.scaleMode = .aspectFit
         
         //BACKGROUND
-        let background = SKSpriteNode(imageNamed: "background_01")
+        let background = SKSpriteNode(imageNamed: "background_02")
         self.scaleMode = .aspectFill
         background.anchorPoint = CGPoint(x: 0, y: 0)
         background.zPosition = Layer.background.rawValue
@@ -30,26 +30,26 @@ class GameScene: SKScene {
         
         // TITLE LABEL 
            gameTitleLabel = SKLabelNode(fontNamed: "ChalkboardSE-Bold") // Replace with your font name
-           gameTitleLabel.text = "Choose Your Game"
+           gameTitleLabel.text = "Let's begin"
            gameTitleLabel.fontSize = 70.0
-           gameTitleLabel.position = CGPoint(x: frame.midX, y: frame.midY + 100)
+           gameTitleLabel.position = CGPoint(x: frame.midX, y: frame.midY + 200)
            gameTitleLabel.zPosition = Layer.ui.rawValue
            addChild(gameTitleLabel)
 
            // Create game one button
            gameOneButton = SKSpriteNode(imageNamed: "gameOneButton") // Replace with your button image name
            gameOneButton.name = "gameOneButton" // Assign a name for identification
-           gameOneButton.position = CGPoint(x: frame.midX - 100, y: frame.midY - 50)
+           gameOneButton.position = CGPoint(x: frame.midX - 300, y: frame.midY - 50)
            gameOneButton.zPosition = Layer.ui.rawValue + 1
-           gameOneButton.setScale(0.2) // Adjust scale if needed
+        gameOneButton.setScale(2.5) // Adjust scale if needed
            addChild(gameOneButton)
 
            // Create game two button (similar to gameOneButton)
            gameTwoButton = SKSpriteNode(imageNamed: "gameTwoButton") // Replace with your button image name
            gameTwoButton.name = "gameTwoButton" // Assign a name for identification
-           gameTwoButton.position = CGPoint(x: frame.midX + 100, y: frame.midY - 50)
+           gameTwoButton.position = CGPoint(x: frame.midX + 300, y: frame.midY - 50)
            gameTwoButton.zPosition = Layer.ui.rawValue + 1
-           gameTwoButton.setScale(0.2) // Adjust scale if needed
+        gameTwoButton.setScale(2.5) // Adjust scale if needed
            addChild(gameTwoButton)
     }
     
